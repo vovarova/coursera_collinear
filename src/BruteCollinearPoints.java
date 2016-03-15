e9eace3676979d401f403605e632ea79c4e42d03
Volodymyr Roman <vova.rova@gmail.com>
2016-03-16 01:00:03 +0200

    first commit

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points){
+        Arrays.sort(points);
+
+
+
+    }    // finds all line segments containing 4 points
+    public           int numberOfSegments() {return  0;}       // the number of line segments
+    public LineSegment[] segments()   {return  null;}             // the line segments
+
+
+
+}
